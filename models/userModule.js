86e445a27b54fcedae995ab69be2b6af07e6a7d3
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2023-10-29 10:19:27 +0530

    Created password encryption method using Brcypt

--- models/userModule.js
+++ models/userModule.js
@@ -1,5 +1,7 @@
 import mongoose,{model} from "mongoose"
+import bcrypt from 'bcrypt'
 
+// creating user schema
 const userSchema = new mongoose.Schema({
     fullName: {
         type: 'String',
@@ -30,6 +32,22 @@ const userSchema = new mongoose.Schema({
 {
     timestamps:true
 })
+
+// for encryption of user password before saving to DB
+userSchema.pre('save', async function (next)  {
+    if(!this.isModified('password')){
+        return next ;
+    }
+
+    // 7 -> no. of rounds of hashing
+    this.password = await bcrypt.hash(this.password,7)
+})
+
+
+// creating methods for userSchema
+// userSchema.methods = {
+
+// }
 
 const user = model('User',userSchema)
 
